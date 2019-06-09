5a7f713b29aee31a60604c3c08c96a21bece5f65
Ameya Salankar <ameya.salankar@kizora.com>
2019-06-09 16:54:36 +0530

    Updates

---
+++ precache-manifest.59e6baa59412bdc1c9b5e463b9b3ac8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6b58d5c59602160e90e8c5487746da4",
+    "url": "/ameyasalankar/index.html"
+  },
+  {
+    "revision": "1112a8aa5a974b7f36bb",
+    "url": "/ameyasalankar/static/css/main.b23f6a49.chunk.css"
+  },
+  {
+    "revision": "5f61e09874c54e12d573",
+    "url": "/ameyasalankar/static/js/2.0ef8dee9.chunk.js"
+  },
+  {
+    "revision": "1112a8aa5a974b7f36bb",
+    "url": "/ameyasalankar/static/js/main.fb651bf1.chunk.js"
+  },
+  {
+    "revision": "4a149244b136ddff30f8",
+    "url": "/ameyasalankar/static/js/runtime~main.e54f851e.js"
+  }
+]);
